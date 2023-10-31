046af376d97ad8631ce93318d423a7e9611a4a08
rajes <rajes@LAPTOP-PB8C76MG>
2023-10-31 20:47:43 +0530

    new Regression Test class

---
+++ src/test/java/practice/RegressionTestForJenkins.java
@@ -0,0 +1,16 @@
+package practice;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import genericUtilities.BaseClass;
+@Listeners(genericUtilities.ListenersImplementationClass.class)
+public class RegressionTestForJenkins extends BaseClass {
+	
+	@Test(groups = "regression")
+	
+	public void regressionDemo() {
+		System.out.println("This is sample Regression Test");
+	}
+
+}
